d60230ac3c0a9062c91d1c9893c064a5c0799a4b
Matthew Rourke <matthewrourke@matthews-air.attlocal.net>
2015-06-08 14:00:51 -0400

    Initialize plugin file

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var through = require('through');
+var path = require('path');
+var glob = require('glob');
+var purify = require('purify-css');
+var gutil = require('gulp-util');
+var File = gutil.File;
+
+const PLUGIN_NAME = 'gulp-purifycss';
+
+module.exports = function(source, styles, destinatin, options) {
+
+  var sourceFiles = [];
+  source.forEach(function(pathPattern) {
+    var files = glob.sync(pathPattern);
+    console.log("Source Files: ", files);
+    sourceFiles = sourceFiles.concat(files);
+  });
+  if (!sourceFiles.length) {
+    throw new PluginError('gulp-purifycss', 'Missing source files for gulp-purifycss');
+  }
+
+
+  var styleFiles = [];
+  styleFiles.forEach(function(pathPattern) {
+    var style = glob.sync(pathPattern);
+    console.log("Style Files: ", style);
+    styleFiles = styleFiles.concat(style);
+  });
+  if (!styleFiles.length) {
+    throw new PluginError('gulp-purifycss', 'Missing style files for gulp-purifycss');
+  }
+
+  var pure = purify(src, styles, {write: false, info: true});
+
+  return through.obj(endStream);
+};
